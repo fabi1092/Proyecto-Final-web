0da97fc98feb5e49fc8e8766c1369082478dfc40
fabi1092 <fabianjosebf@gmail.com>
2026-06-15 14:23:21 -0400

    aksjdfkdshebgf

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+
+// 1. IMPORTAMOS TODAS LAS FUNCIONES SUELTAS (Agregamos las dos nuevas)
+const { register, login, resetPassword, solicitarCodigo } = require('../controllers/authController');
 
 // Rutas Públicas
 router.post('/register', register);
 router.post('/login', login);
-router.put('/reset', authController.resetPassword);
-router.post('/solicitar-codigo', authController.solicitarCodigo);
 
+// 2. LAS USAMOS DIRECTAMENTE (Igual que el login y register)
+router.put('/reset', resetPassword);
+router.post('/solicitar-codigo', solicitarCodigo);
 
 module.exports = router;
